e0388d5f8cfdbfa83dc423aee4b270f2b2e553f8
Junaid-khatri <junaidkhatri102@gmail.com>
2024-08-22 11:36:51 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Profile from './component/Profile';
 
 function App() {
   return (
-    <div className="App">
+    <div >
      <Profile/>
     </div>
   );
--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,30 +1,32 @@
 import React, { useEffect, useRef, useState } from "react";
-import Icon from '@mdi/react';
-import { mdiPencil, mdiCheck } from '@mdi/js';
+import Icon from "@mdi/react";
+import { mdiPencil, mdiCheck } from "@mdi/js";
 import "./profile.css";
+import pp from "./Assests/background-image.webp";
 
 const Profile = () => {
   const [profile, setprofile] = useState({ name: "", about: "" });
-  const [icon,setIcon] = useState(mdiPencil);
   const editName = useRef(null);
   const editAbout = useRef(null);
-  
+  const [nameIcon, setNameIcon] = useState(mdiPencil);
+  const [aboutIcon, setAboutIcon] = useState(mdiPencil);
 
   useEffect(() => {
     const name = localStorage.getItem("name");
     const about = localStorage.getItem("about");
-    setprofile({ name: name || "", about: about || "hey guy's i am using nandu's chat application" });
+    setprofile({
+      name: name || "",
+      about: about || "hey guy's i am using nandu's chat application",
+    });
   }, []);
 
   const handleChange = (e, item) => {
     if (item === "name") {
-      console.log("updating name");
       setprofile((prevState) => ({
         ...prevState,
         name: e.target.value,
       }));
     } else if (item === "about") {
-      console.log("updating about");
       setprofile((prevState) => ({
         ...prevState,
         about: e.target.value,
@@ -32,64 +34,87 @@ const Profile = () => {
     }
   };
 
-  const edit = (inputRef) =>{
-    console.log("button clicked");
-    
-    if(inputRef.current){
-        inputRef.current.readOnly = !inputRef.current.readOnly;
-        if(icon ===mdiPencil){
-            setIcon(mdiCheck);
+  const edit = (inputRef) => {
+    if (inputRef.current) {
+      inputRef.current.readOnly = !inputRef.current.readOnly;
+
+      if (inputRef.current.readOnly) {
+        inputRef.current.blur();
+      } else if (!inputRef.current.readOnly) {
+        inputRef.current.focus();
+      }
+
+      if (inputRef.current.name === "name") {
+        if (nameIcon === mdiPencil) {
+          setNameIcon(mdiCheck);
+        } else if (nameIcon === mdiCheck) {
+          setNameIcon(mdiPencil);
         }
-        else if(icon ===mdiCheck){
-            setIcon(mdiPencil)
+      } else if (inputRef.current.name === "about") {
+        if (aboutIcon === mdiPencil) {
+          setAboutIcon(mdiCheck);
+        } else if (aboutIcon === mdiCheck) {
+          setAboutIcon(mdiPencil);
         }
+      }
     }
   };
 
   return (
-    <div className="profilecontainer">
-      <div className="first">
-        <h2>Profile</h2>
+    <div className="profile-container">
+      <h2>Profile</h2>
+      <div className="pp-pic">
+        <img className="profile-pic" src={pp} alt="not-found" />
       </div>
-      <div className="profilepic">
-        <img src="/Assets/background-image.webp" alt="Profile Background" />
+      <div>
+        <div className="name-heading">
+          <p>Your Name</p>
+        </div>
+        <div className="name">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            ref={editName}
+            readOnly={true}
+            value={profile.name}
+            onChange={(event) => handleChange(event, "name")}
+          />
+
+          <span>
+            <button onClick={() => edit(editName)}>
+              <Icon path={nameIcon} size={0.8} />
+            </button>
+          </span>
+        </div>
       </div>
-      <div className="detailcontainer">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          ref={editName}
-          readOnly={true}
-          value={profile.name}
-          onChange={(event) => handleChange(event, "name")}
-        />
-         <button onClick={()=>edit(editName)}><Icon path={icon} size={0.8} /></button>
-         
-      </div>
-      <div className="infocontainer">
+      <br></br>
+      <div className="about">
         <p>
-          This Name is not your name or pin. This name will be visible to your
-          chatApp friends.
+          This is not your Username or PIN. This name will be visible to your
+          WhatsApp contacts.
         </p>
       </div>
-      <div className="detailcontainer">
-        <label htmlFor="about">About</label>
-        <input
-          type="text"
-          name="about"
-          id="about"
-          ref={editAbout}
-          readOnly={true}
-          value={profile.about}
-          onChange={(event) => handleChange(event, "about")}
-        />
-        <button onClick={()=>edit(editAbout)}><Icon path={mdiPencil} size={0.8} /></button>
-         
-      </div>
-      <div className="infocontainer">
-        <p>This detail will be visible to your chatApp friends.</p>
+      <div>
+        <div className="name-heading">
+          <p>About</p>
+        </div>
+        <div className="name">
+          <input
+            type="text"
+            name="about"
+            id="about"
+            ref={editAbout}
+            readOnly={true}
+            value={profile.about}
+            onChange={(event) => handleChange(event, "about")}
+          />
+          <span>
+            <button onClick={() => edit(editAbout)}>
+              <Icon path={aboutIcon} size={0.8} />
+            </button>
+          </span>
+        </div>
       </div>
     </div>
   );
